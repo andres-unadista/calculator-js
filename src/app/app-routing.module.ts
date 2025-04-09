df75d888b76c99045141cd485187ba7881ad6195
ANDRES FELIPE FERNANDEZ GARCIA <affernandez999@gmail.com>
2025-04-09 16:44:53 -0500

    modify app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginGuard } from './guards/login.guard';
 
 const routes: Routes = [
   {
